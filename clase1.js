9c34b3510248b8cd71aa04a73e369247e14ff4bc
cdominguez_axity <cdominguez.bravo@gmail.com>
2020-04-30 22:47:00 -0400

    operando con numeros en js

--- clase1.js
+++ clase1.js
@@ -12,3 +12,32 @@ var cantidadDeLetasDelNombre = nombre.length;
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
 
 var str = nombre.substr(1, 2);
+
+// https://platzi.com/clases/1339-fundamentos-javascript/12888-variables-numer-8/
+
+var edad = 27;
+edad += 1;
+
+var peso = 75;
+
+peso -= 2;
+
+var sandwich = 1;
+
+sandwich = peso + sandwich;
+
+var jugarAfutbol = 3;
+
+peso -= jugarAfutbol;
+
+var precioDeVino = 200.3;
+
+var total = Math.round(precioDeVino * 100 * 3) / 100;
+
+var totalStr = total.toFixed(3);
+
+var total2 = parseFloat(totalStr);
+
+var pizza = 8;
+var persona = 2;
+var cantidadDePorcionesPorPersna = pizza / persona;
